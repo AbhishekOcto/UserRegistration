30d3d1b0a29cbf692759eeb587282bc88a60ac52
Abhishek <you@example.com>
2022-09-28 19:44:45 +0530

    [Abhishek]Added: UC11 Junit paramaterized test

---
+++ UserRegistration/src/main/java/com/bridgelabz/userReg/InvalidRegistrationException.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.userReg;
+
+public class InvalidRegistrationException extends Exception{
+    public InvalidRegistrationException(String message) {
+        super(message);
+    }
+}
